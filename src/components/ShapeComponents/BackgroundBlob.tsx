cdeb4bed535e88e0254467293bdf60335046d38e
vincentropy <29819815+vincentropy@users.noreply.github.com>
2021-11-15 18:01:07 +0000

    added background shapes

--- src/components/ShapeComponents/BackgroundBlob.tsx
+++ src/components/ShapeComponents/BackgroundBlob.tsx
@@ -1,23 +1,22 @@
 import { ShapeComponent, ShapeKind, StyleProps } from '../../lib/shapelib';
-import styles from './BackgroundBlob.module.css'
+import styles from './BackgroundBlob.module.css';
 
 type BackgroundBlobProps = StyleProps & {
   numPoints: number;
 };
 
 export const BackgroundBlob: React.FC<BackgroundBlobProps> = (props) => {
-  const {numPoints, ...passThroughProps} = props
+  const { numPoints, ...passThroughProps } = props;
   const shapeProps = {
     kind: ShapeKind.CLOSED,
     complexity: numPoints,
+    variability: 60,
   };
 
   return (
     <div className={styles.container}>
       <ShapeComponent {...shapeProps} {...passThroughProps} className={styles.shape} />
-      <div className={styles.contents}>
-      {props.children}
-      </div>
+      <div className={styles.contents}>{props.children}</div>
     </div>
   );
 };
