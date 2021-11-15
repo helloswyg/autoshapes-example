9a2707c12d45616e15e9552c0a7e94415750cc93
vincentropy <29819815+vincentropy@users.noreply.github.com>
2021-11-15 14:00:01 +0000

    WIP: various changes

--- src/components/AccentBlobsExample/AccentBlobsExample.tsx
+++ src/components/AccentBlobsExample/AccentBlobsExample.tsx
@@ -1,5 +1,5 @@
-import { Typography } from '@mui/material';
-import { ReactNode } from 'react';
+import { Grid, Paper, Paper, Typography } from '@mui/material';
+import { ReactNode, FC } from 'react';
 import { ShapeComponent, ShapeKind } from '../../lib/shapelib';
 import { simpleLinearGradient } from '../../lib/shapelib/utils';
 
@@ -14,13 +14,25 @@ const shapeProps = {
   kind: ShapeKind.CLOSED,
   complexity: 4,
   stroke: { color: '#ff1b5f', width: 3 },
-  fill: simpleLinearGradient(['red', 'blue'])
+  fill: simpleLinearGradient(['red', 'blue']),
 };
 
+const Item: FC<{}> = props => (
+  <Paper>{props.children}</Paper>
+)
+
 export const AccentBlobsExample = () => {
-  return ( 
+  return (
     <div className="example">
-      <ShapeComponent {...shapeProps} style={{ position: 'absolute', maxWidth: '80%', marginTop: '-3em' }} />
+      {/* <ShapeComponent {...shapeProps} style={{ position: 'absolute', maxWidth: '80%', marginTop: '-3em' }} /> */}
+      <Grid container spacing={2}>
+  <Grid item xs={8}>
+item1
+  </Grid>
+  <Grid item xs={8}>
+    item 2
+  </Grid>
+</Grid>
     </div>
   );
 };
