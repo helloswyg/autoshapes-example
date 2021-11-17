e901c62e55cd5c17d8d181487bfe9bb2f98f959c
vincentropy <29819815+vincentropy@users.noreply.github.com>
2021-11-17 17:09:03 +0000

    implement smoothness for blobs

--- src/components/ShapeComponents/BackgroundBlob.tsx
+++ src/components/ShapeComponents/BackgroundBlob.tsx
@@ -1,4 +1,4 @@
-import { ShapeComponent, ShapeKind, StyleProps } from '../../lib/shapelib';
+import { PathProps, ShapeComponent, ShapeKind, StyleProps } from '../../lib/shapelib';
 import styles from './BackgroundBlob.module.css';
 
 type BackgroundBlobProps = StyleProps & {
@@ -7,15 +7,16 @@ type BackgroundBlobProps = StyleProps & {
 
 export const BackgroundBlob: React.FC<BackgroundBlobProps> = (props) => {
   const { numPoints, ...passThroughProps } = props;
-  const shapeProps = {
+  const pathProps: PathProps= {
     kind: ShapeKind.CLOSED,
     complexity: numPoints,
-    variability: 60,
+    variability: 50,
+    smoothness: .8
   };
 
   return (
     <div className={styles.container}>
-      <ShapeComponent {...shapeProps} {...passThroughProps} className={styles.shape} />
+      <ShapeComponent {...pathProps} {...passThroughProps} className={styles.shape} />
       <div className={styles.contents}>{props.children}</div>
     </div>
   );
