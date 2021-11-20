c08f3242ca0bddbb2ea5efad0498664a9eadfd46
vincentropy <29819815+vincentropy@users.noreply.github.com>
2021-11-20 13:49:46 +0000

    Demo Component for grammar

---
+++ src/components/ShapeComponents/VariableLine.stories.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { VariableLine } from './VariableLine';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Shape Lib/Variable Line',
+  component: VariableLine,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    stroke: { control: 'color' },
+  },
+} as ComponentMeta<typeof VariableLine>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof VariableLine> = (args) => (
+  <div style={{ width: '30rem', height: '30rem', position:'relative'}}>
+    <VariableLine {...args} />
+  </div>
+);
+
+export const FromStringShort = Template.bind({});
+FromStringShort.args = {
+    pathString: 'BBaa',
+    stroke:'#000000'
+
+};
+
+export const FromStringLong = Template.bind({});
+FromStringLong.args = {
+    pathString: 'BFIBSFabcabcabc',
+    stroke:'#000000'
+
+};
---
+++ src/components/ShapeComponents/VariableLine.tsx
@@ -0,0 +1,19 @@
+import { ShapeComponent, ShapeKind, ShapeProps } from "../../lib/shapelib";
+import { decodePathString, evaluateTree } from "../../lib/shapelib/grammar";
+import styles from './VariableLine.module.css'
+
+interface VariableLineProps extends Partial<ShapeProps>{
+    pathString: string
+}
+
+export const VariableLine: React.FC<VariableLineProps> = props => {
+    const tree = decodePathString(props.pathString)
+    const path = evaluateTree(tree)
+    return <ShapeComponent 
+    {...props} 
+    path={path}
+    kind={ShapeKind.LOOPY}
+    className={styles.shape} 
+    fill="none"
+    />
+}
