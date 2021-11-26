ffd181b390a706fb6f0a44f98a26fc01e57e9fef
vincentropy <29819815+vincentropy@users.noreply.github.com>
2021-11-26 20:41:18 +0000

    fix storybook / mui theming

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,5 +7,11 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app"
-  ]
+  ],
+  webpackFinal(config) { // workaround from https://github.com/mui-org/material-ui/issues/24282#issuecomment-951015101 
+    delete config.resolve.alias['emotion-theming'];
+    delete config.resolve.alias['@emotion/styled'];
+    delete config.resolve.alias['@emotion/core'];
+    return config;
+  },
 }
